251e72bfe5cacf711857707b736dc25acbf49179
mac <chiamakaesther098@gmail.com>
2026-03-03 10:57:55 +0100

    updt

--- src/App.js
+++ src/App.js
@@ -10,7 +10,17 @@ const firstName = "Chiamaka"; // change or leave empty string ""
 const App = () => {
   return (
     <div className=" justify-content-center align-items-center vh-100 bg-custom m-2 w-full ">
-      <Card style={{ width: "20rem", textAlign: "center", padding: "1rem" , marginLeft:"32rem", borderRadius:"2rem"}}>
+      <Card
+        style={{
+          width: "20rem",
+          textAlign: "center",
+          padding: "1rem",
+          marginLeft: "43rem",
+          borderRadius: "2rem",
+          marginBottom: "12rem",
+          marginTop: "4rem",
+        }}
+      >
         <Image />
         <Card.Body>
           <Name />
@@ -20,14 +30,12 @@ const App = () => {
       </Card>
 
       <div className="mt-4 text-center w-100">
-        <h3>
-          {firstName ? `Hello, ${firstName}!` : "Hello, there!"}
-        </h3>
+        <h3>{firstName ? `Hello, ${firstName}!` : "Hello, there!"}</h3>
         {firstName && (
           <img
             src="https://media.istockphoto.com/id/1353379172/photo/cute-little-african-american-girl-looking-at-camera.jpg?s=170667a&w=0&k=20&c=-1tor8503hrXieYanOw1FmAY01phUtobZ46pC-1_XD8="
             alt="user"
-            className="rounded-pill mt-2"
+            className="rounded-pill w-10 h-10 mt-2"
           />
         )}
       </div>
